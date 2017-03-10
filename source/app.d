dcafb3bb6fb018023ee103da6fba16a6c1f53224
Radek Zieliński <radoslaw@rzielinski.pl>
2017-03-10 12:02:42 +0100

    Add starting files

---
+++ source/app.d
@@ -0,0 +1,15 @@
+import std.stdio;
+
+void main()
+{
+	/*
+	 * TODO:
+	 * - create UI.
+	 * - make sure that it works on Windows also
+	 * - add multiple loacles
+	 * - add possibility to backup online
+	 * - some icon will be needed
+	 *
+	 */
+	writeln("Edit source/app.d to start your project.");
+}
---
+++ source/config.d
@@ -0,0 +1,18 @@
+module config;
+
+/**
+ * Config
+ */
+class Config
+{
+	private string _dbFileAddr;
+	/*
+	 * TODO:
+	 * when needed add missing fields;
+	 */
+
+	this()
+	{
+		
+	}
+}
